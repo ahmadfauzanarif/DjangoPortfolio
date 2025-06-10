22d9125768fff9fd43a19540e946dc4b8f320136
ahmadfauzanarif <ahmadfauzanarif3@gmail.com>
2025-06-10 20:39:12 +0700

    Penambahan Fitur CRUD pada Model, ordering, filtering dan sorting

--- profil/admin.py
+++ profil/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Barang, Jenis
+
+class BarangAdmin(admin.ModelAdmin):
+    list_display = ['kdbrg', 'namabrg', 'stok', 'harga', 'jenis_id', 'waktu_posting', 'link_gbr']
+    search_fields = ('kdbrg', 'namabrg', 'jenis_id__nama') # __nama digunakan untuk memilih kolom mana yang akan dipakai untuk relasi dan ditampilkan
+    list_filter = ('jenis_id',)
+    list_per_page = 3
+    ordering = ('stok',)
+
+admin.site.register(Barang, BarangAdmin)
+admin.site.register(Jenis)
